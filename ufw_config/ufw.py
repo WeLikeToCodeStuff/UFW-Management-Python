6d738feb4a0089eb96581fbf21fbc71a18e54213
Jamie L <51766327+DeveloperNeon@users.noreply.github.com>
2020-11-21 20:53:17 -0600

    Added portrange option

--- ufw_config/ufw.py
+++ ufw_config/ufw.py
@@ -18,20 +18,30 @@ def main():
     generator = UfwConfigGenerator()
 
     acceptdeny = input(
-        "Would you like to deny or to accept a port? Type deny to deny and accept to accept: "
+        "Would you like to accept or deny a port? \n(You can also type portrange to specify a portrange to accept/deny): "
     )
     if acceptdeny == "accept":
         acceptport = input("What port would you like to accept?: ")
         generator.generate(acceptport, True)
-        print(f"Port '{acceptport}' opened!")
+        print(f"Port '{acceptport}' has been set to allowed")
         exit()
 
     elif acceptdeny == "deny":
         denyport = input("What port would you like to deny?: ")
         generator.generate(denyport, True)
-        print(f"Port '{denyport}' closed!")
+        print(f"Port '{denyport}' has been set to denied")
         exit()
-
+    elif acceptdeny == "portrange":
+        rangenum = input("Please type a port range to accept/deny. \nUse the format: startport:stopport Example: 100:200 :")
+        rangestatus = input("Would you like to accept or deny this port range?: ")
+        rangetype = input("What type of these ports? tcp/udp :")
+        rangetypelower = rangetype.lower()
+        if rangestatus == "accept":
+            os.system("sudo ufw " + rangestatus + " " + rangenum + " /" + rangetype)
+            print(f"Port range '{rangenum}' has been set to accepted")
+        elif rangestatus == "deny":
+            os.system("sudo ufw " + rangestatus + " " + rangenum + " /" + rangetype)
+            print(f"Port range '{rangenum}' has been set to denied")
     else:
-        print("That's not a valid option. Please choose 'accept' or 'deny'")
+        print("That's not a valid option. Please choose 'accept', 'deny', or 'portrange'")
         exit()
